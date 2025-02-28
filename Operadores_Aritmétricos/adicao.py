702e1c883984398775945ee3093d0961af2241c5
Tibbas <166184211+Tibbas@users.noreply.github.com>
2025-02-28 20:26:54 -0300

    Initial commit

---
+++ Operadores_Aritmétricos/adicao.py
@@ -0,0 +1,6 @@
+x = (int(input('Insira o 1ºnum:')))
+y = (int(input('Insira o 2ºnum:')))
+
+soma = x + y
+
+print('A soma é',soma)
---
+++ subtracao.py
@@ -0,0 +1,6 @@
+x = (int(input('Insira o 1ºnum:')))
+y = (int(input('Insira o 2ºnum:')))
+
+sub = x - y
+
+print('A subtracao é',sub)
